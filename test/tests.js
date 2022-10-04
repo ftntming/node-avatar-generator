bd065cb6976659de79b14bcac157fa87e4bb47ec
ftntming <ftntming@gmail.com>
2022-10-03 20:35:33 -0700

    Rename

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,7 @@ function importTest(name, path) {
     describe(name, require(path));
 }
 
-describe("initials-avatar-generator", function () {
+describe("node-avatar-generator", function () {
     importTest("Configuration", './configuration/configuration_tests');
     importTest("Font", './font/font_tests');
 });
